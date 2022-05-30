012daa6d6db97780985e6302c8b6169fb0af466c
renan.oliveira <renan.oliveira.rao@gmail.com>
2022-05-30 18:58:59 -0300

    Creates account class

---
+++ src/main/kotlin/one/digitalinnovation/bank/Account.kt
@@ -0,0 +1,17 @@
+package one.digitalinnovation.bank
+
+import java.math.BigDecimal
+
+class Account(
+    val agency: String,
+    val number: String,
+    val balance: BigDecimal
+) {
+    fun deposit(value: BigDecimal){
+
+    }
+
+    fun withdraw(value: BigDecimal){
+
+    }
+}
