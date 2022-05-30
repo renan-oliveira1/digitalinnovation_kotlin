4ccf89596b8ec331cffdebaaa30c26eed131d588
renan.oliveira <renan.oliveira.rao@gmail.com>
2022-05-30 18:36:46 -0300

    Created person and main class

---
+++ src/main/kotlin/one/digitalinnovation/bank/Main.kt
@@ -0,0 +1,8 @@
+package one.digitalinnovation.bank
+
+fun main(){
+    val people = People()
+
+    println(people.name)
+    println(people.cpf)
+}
--- src/main/kotlin/one/digitalinnovation/bank/People.kt
+++ src/main/kotlin/one/digitalinnovation/bank/People.kt
@@ -1,4 +1,6 @@
 package one.digitalinnovation.bank
 
 class People {
+    var name: String = "Jether"
+    var cpf: String = "123.123.123-11"
 }
