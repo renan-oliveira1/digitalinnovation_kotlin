b83249448a1c156773df8681e3369a9c0a637cfc
renan.oliveira <renan.oliveira.rao@gmail.com>
2022-05-30 18:46:34 -0300

    Class Address creates, and apply private set cpf properties

--- src/main/kotlin/one/digitalinnovation/bank/Main.kt
+++ src/main/kotlin/one/digitalinnovation/bank/Main.kt
@@ -2,7 +2,9 @@ package one.digitalinnovation.bank
 
 fun main(){
     val people = People()
+    people.name ="1231"
 
     println(people.name)
     println(people.cpf)
+    println(people.Address().street)
 }
--- src/main/kotlin/one/digitalinnovation/bank/People.kt
+++ src/main/kotlin/one/digitalinnovation/bank/People.kt
@@ -2,5 +2,11 @@ package one.digitalinnovation.bank
 
 class People {
     var name: String = "Jether"
+
     var cpf: String = "123.123.123-11"
+    private set
+
+    inner class Address{
+        var street: String = "Rua teste"
+    }
 }
